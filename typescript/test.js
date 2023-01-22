cb4ca255dce57825307d790a458f85e5daf8482d
Yoon Lee <poordobby@naver.com>
2023-01-23 02:40:22 +0900

    types

--- typescript/test.js
+++ typescript/test.js
@@ -1,18 +1,13 @@
+"use strict";
 // const a = "LeeTrue";
-
 let a = "Leeture";
-
 // a = 3; // Type 'number' is not assignable to type 'string'
-
 /* -----------------
  TYPE ANNOTATION
 ----------------- */
-
-let b: string;
+let b;
 b = "leetrue";
 // b = 39; // Type 'number' is not assignable to type 'string'.
-
-function hello(b: number) {}
-
+function hello(b) { }
 hello(33);
 // hello("leetrue"); // Argument of type 'string' is not assignable to parameter of type 'number'.
