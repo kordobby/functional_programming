bede3d25470efd1db84b8e7ad8d5808db508c8e2
Yoon Lee <poordobby@naver.com>
2023-01-22 15:06:08 +0900

    typescript

--- typescript/test.ts
+++ typescript/test.ts
@@ -3,3 +3,16 @@
 let a = "Leeture";
 
 a = 3; // Type 'number' is not assignable to type 'string'
+
+/* -----------------
+ TYPE ANNOTATION
+----------------- */
+
+let b: string;
+b = "leetrue";
+b = 39; // Type 'number' is not assignable to type 'string'.
+
+function hello(b: number) {}
+
+hello(33);
+hello("leetrue"); // Argument of type 'string' is not assignable to parameter of type 'number'.
